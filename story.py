ad97d538c0850a73d31f59e6fe35aa7bf5ca0c36
Jaden Ziga <179839509+jaden313@users.noreply.github.com>
2025-09-17 23:35:40 -0400

    Added picking up the sword and a dragon appears.

--- story.py
+++ story.py
@@ -12,6 +12,7 @@ def intro():
 
 def left_path():
     print("You walk left and find a mysterious glowing sword stuck in a stone.")
+    print("You pick up the sword and a fierce dragon appears.")
 
 def right_path():
     print("You walk right and encounter a talking squirrel who challenges you to a duel.")
